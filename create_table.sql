07b139be4a0d04ae3016426761bfd5ea9621e8b4
jthueringer <jthueringer@users.noreply.github.com>
2017-05-26 22:40:03 +0200

    Update create_table.sql
    für die gewünschte Erweiterbarkeit habe ich noch eine extra Relation eingefügt für "handle" - dann kann der zusammengesetzte PRIMARY KEY in tweet eben aus handle_id und timeTweeted bestehen und damit noch mehr handles zulassen, ohne dass wirklich mehr daten gespeichert werden müssen
    
    Einverstanden?

--- create_table.sql
+++ create_table.sql
@@ -1,8 +1,16 @@
+DROP TABLE IF EXISTS handle CASCADE;
+CREATE TABLE handle (
+	handle_id integer NOT NULL,
+	handle_name varchar NOT NULL
+);
+
+
 DROP TABLE IF EXISTS tweet CASCADE;
 CREATE TABLE tweet (
+	handle_id integer
+		references handle(handle_id),
 	timeTweeted date NOT NULL,
-	handle boolean NOT NULL,
-	PRIMARY KEY(timeTweeted, handle),
+	PRIMARY KEY(timeTweeted, handle_id),
 	favouriteCount integer NOT NULL,
 	retweetCount integer,
 	text varchar NOT NULL,
@@ -17,11 +25,9 @@ CREATE TABLE hashtag (
 DROP TABLE IF EXISTS has CASCADE;
 CREATE TABLE has (
 	timeTweeted date NOT NULL,
-	handle boolean NOT NULL,
+	handle_id integer NOT NULL,
 	FOREIGN KEY (timeTweeted, handle) 
 		references tweet(timeTweeted, handle),
 	tag varchar
 		references hashtag(tag)
 );
-
-
