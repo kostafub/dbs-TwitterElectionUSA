5e9651fffddbba4198fd8a8676fdd86470df8eb5
jthueringer <jthueringer@users.noreply.github.com>
2017-05-28 14:54:20 +0200

    date in timeTweeted zu timestamp geändert

--- create_table.sql
+++ create_table.sql
@@ -1,33 +1,33 @@
 DROP TABLE IF EXISTS handle CASCADE;
-CREATE TABLE handle (
-	handle_id integer NOT NULL,
-	handle_name varchar NOT NULL
+CREATE TABLE IF NOT EXISTS handle (
+	handle_id integer PRIMARY KEY,
+	handle_name varchar
 );
 
 
 DROP TABLE IF EXISTS tweet CASCADE;
-CREATE TABLE tweet (
+CREATE TABLE IF NOT EXISTS tweet (
 	handle_id integer
 		references handle(handle_id),
-	timeTweeted date NOT NULL,
+	timeTweeted timestamp NOT NULL,
 	PRIMARY KEY(timeTweeted, handle_id),
-	favouriteCount integer NOT NULL,
+	favouriteCount integer,
 	retweetCount integer,
 	text varchar NOT NULL,
 	originalAuthor varchar
 );
 
 DROP TABLE IF EXISTS hashtag CASCADE;
-CREATE TABLE hashtag (
+CREATE TABLE IF NOT EXISTS hashtag (
 	tag varchar PRIMARY KEY
 );
 
 DROP TABLE IF EXISTS has CASCADE;
-CREATE TABLE has (
-	timeTweeted date NOT NULL,
+CREATE TABLE IF NOT EXISTS has (
+	timeTweeted timestamp NOT NULL,
 	handle_id integer NOT NULL,
-	FOREIGN KEY (timeTweeted, handle) 
-		references tweet(timeTweeted, handle),
+	FOREIGN KEY (timeTweeted, handle_id) 
+		references tweet(timeTweeted, handle_id),
 	tag varchar
 		references hashtag(tag)
 );
