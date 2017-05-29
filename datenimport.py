be004ae66777c455b13701365df6faeeaa80049b
jthueringer <jthueringer@users.noreply.github.com>
2017-05-29 14:25:23 +0200

    Update datenimport.py

--- datenimport.py
+++ datenimport.py
@@ -1,51 +1,32 @@
 # *.xlsx laden und lesen
 from openpyxl import load_workbook
 # Verbindung zur Datenbank und string-escape für sql
-import pg 
-pg=pg.connect(dbname='election', user='julez', passwd='haha')
-
+from pgdb import connect
+try:
+	pg=connect(dbname='election', user='julez', password='haha')
+except:
+	print('keine db-verbindung')
 # workbook mit aufbereiteten Daten laden
 wb = load_workbook('american-election-repair.xlsx')
 ws = wb['bereinigt']
 wslen = ws.max_row-1
 
-# Hilfsfunktion ist_enthalten
-def ist_enthalten (cell_value, liste):
-	lenListe = len(liste)
-	for name in range(0,lenListe):
-		if cell_value==liste[name]:
-			return True
-		else:
-			continue
-		return False
-		
 # insert in relation handle
-# projektion der möglichen handles
-values = []
-ids = []
-inhalt = []
-zaehler = 0
+cursor = pg.cursor()
 for row in range(0, wslen):
-	cell_handle_id = "{col}{row}".format(col='A', row=(row+2))
-	cell_handle = "{col}{row}".format(col='B', row=(row+2))
-	if (zaehler == 0):
-		ids = [ws[cell_handle_id].value]
-		values = [ws[cell_handle].value]
-		inhalt = [str('INSERT INTO handle VALUES ('+ str(ws[cell_handle_id].value) + ', '+ str('\'' + ws[cell_handle].value) + '\')')]
-		zaehler +=1
+	cell_handle = "{col}{row}".format(col='K', row=(row+2))
+	if (str(ws[cell_handle].value) == 'None'):
+		break
 	else:
-		if (ist_enthalten(ws[cell_handle_id].value, ids)):
-			continue
-		else:
-			ids.append(ws[cell_handle_id].value)
-			values.append(ws[cell_handle].value)
-			zaehler +=1
-			inhalt.append(str('INSERT INTO handle VALUES ('+ str(ws[cell_handle_id].value) + ', '+ str('\'' + ws[cell_handle].value) + '\')'))
-for eintrag in range(0, len(ids)):
-	#print(inhalt[eintrag])
-	#pg.query(inhalt[eintrag])
+		data_handle = [row, ws[cell_handle].value]
+
+	format_str = "INSERT INTO handle VALUES (%s, %s)"
+	cursor.execute(format_str, data_handle)
+pg.commit()
+cursor.close()
 
 # insert in relation tweet
+cursor = pg.cursor()
 for row in range(0, wslen):
 	cell_handle_id = "{col}{row}".format(col='A', row=(row+2))
 	cell_time = "{col}{row}".format(col='F', row=(row+2))
@@ -53,30 +34,43 @@ for row in range(0, wslen):
 	cell_retw = "{col}{row}".format(col='H', row=(row+2))
 	cell_text = "{col}{row}".format(col='C', row=(row+2))
 	cell_auth = "{col}{row}".format(col='I', row=(row+2))
-	inhalt = str('INSERT INTO handle VALUES ('+ str(ws[cell_handle_id].value) + ', ' + str(ws[cell_time].value)+ ', ' + str(ws[cell_fav].value) + ', '+ str(ws[cell_retw].value)+ ', \''+ pg.escape_string(str(ws[cell_text].value)) + '\', '+ str(ws[cell_auth].value) + '\')')
-	#print(inhalt)
-	#pg.query(inhalt)
+	
+	format_str = "INSERT INTO tweet VALUES (%s, %s, %s, %s, %s, %s)"
+	data_tweet = [ws[cell_handle_id].value, ws[cell_time].value, ws[cell_fav].value, ws[cell_retw].value, ws[cell_text].value, ws[cell_auth].value]
+	cursor.execute(format_str, data_tweet)
+pg.commit()
+cursor.close()
+
 
 # insert in relation hashtag
+cursor = pg.cursor()
 for row in range(0, wslen):
-	cell_tag_id = "{col}{row}".format(col='D', row=(row+2))
-	cell_tag = "{col}{row}".format(col='E', row=(row+2))
-	if (str(ws[cell_tag].value) == 'NULL'):
-		inhalt = str('INSERT INTO handle VALUES ('+ str(ws[cell_tag_id].value) + ', '+ pg.escape_string(str(ws[cell_tag].value)) + ')')
+	cell_tag = "{col}{row}".format(col='J', row=(row+2))
+	if (str(ws[cell_tag].value) == 'None'):
+		break
 	else:
-		inhalt = str('INSERT INTO handle VALUES ('+ str(ws[cell_tag_id].value) + ', '+ str('\'' + ws[cell_tag].value) + '\')')
-	#print(inhalt)
-	#pg.query(inhalt)
+		data_tag = [row, ws[cell_tag].value]
+	
+	format_str = "INSERT INTO hashtag VALUES (%s, %s)"
+	cursor.execute(format_str, data_tag)
+pg.commit()
+cursor.close()
 
 # insert in relation has
+cursor = pg.cursor()
 for row in range(0, wslen):
 	cell_time = "{col}{row}".format(col='F', row=(row+2))
 	cell_handle_id = "{col}{row}".format(col='A', row=(row+2))
 	cell_tag_id = "{col}{row}".format(col='D', row=(row+2))
-	inhalt = str('INSERT INTO handle VALUES ('+ str(ws[cell_time].value) + ', '+ str(ws[cell_handle_id].value) + ', ' + str(ws[cell_tag_id].value) + ')')
-	#print(inhalt)
-	#pg.query(inhalt)
+	tag_id_list = str(ws[cell_tag_id].value)
 	
-	
-	
+	if (tag_id_list == 'None'):
+		continue
+	else:
+		for id in tag_id_list.split():
+			format_str = "INSERT INTO has VALUES (%s, %s, %s)"
+			data_has = [ws[cell_time].value, ws[cell_handle_id].value, id]
+			cursor.execute(format_str, data_has)
+pg.commit()
+cursor.close()
 	
